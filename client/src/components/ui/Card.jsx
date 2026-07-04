1677e76fa4094083102e537c192f4c36e7220d19
Samira Ahmed <samiraahmed9913@gmail.com>
2026-07-05 04:06:20 +0600

    made some small changes in card

--- client/src/components/ui/Card.jsx
+++ client/src/components/ui/Card.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const Card = ({title, children}) => {
+const Card = ({title, children,  className = "" }) => {
   return (
-    <div className="bg-[#30302e] rounded-2xl p-6 shadow-md w-full border-white/16 border">
+    <div className="bg-[#30302e] rounded-2xl mb-4 p-8 shadow-md w-full border-white/16 border ${className}">
       {/* Header */}
-      <h2 className="text-white text-lg font-semibold mb-4">
-        {title}
-      </h2>
+      {title && (
+        <h2 className="text-white text-lg font-semibold mb-4">
+          {title}
+        </h2>
+      )}
 
-      {/* Body (dynamic content) */}
+      {/* Body */}
       <div>{children}</div>
     </div>
   )
