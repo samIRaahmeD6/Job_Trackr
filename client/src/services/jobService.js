fdf67d0ca8c4dbf50dc16f03d12cd3f0e589155c
Samira Ahmed <samiraahmed9913@gmail.com>
2026-07-07 22:52:08 +0600

    added jobService for connecting backend with frontend

--- client/src/services/jobService.js
+++ client/src/services/jobService.js
@@ -6,12 +6,20 @@ export const createJob = async (data) => {
   return response.data;
 };
 
+// GET ALL JOBS
+export const getJob = async () => {
+  const response = await api.get("/jobs");
+  return response.data;
+};
+
+// GET JOB STATS
 export const showJobStats = async () => {
   const response = await api.get("/jobs/stats");
   return response.data;
 };
 
-export const getJob = async() =>{
-  const response = await api.get("jobs");
-  return response.data
-}
+// UPDATE JOB STATUS
+export const updateJobStatus = async (id, status) => {
+  const response = await api.patch(`/jobs/${id}/status`, { status });
+  return response.data;
+};
