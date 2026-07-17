a4d3aa59c87d419ee0daa6e7d497a28c5c04bde8
Samira Ahmed <samiraahmed9913@gmail.com>
2026-07-17 21:05:57 +0600

    added export CSV file option

--- client/src/components/layout/Topbar.jsx
+++ client/src/components/layout/Topbar.jsx
@@ -1,34 +1,42 @@
 import Button from "../ui/Button";
 import AddJobModal from "../../features/jobs/AddJobModal";
 import { useState } from "react";
-const Topbar = ({ title, subTitle}) => {
+import { exportJobsToCSV } from "../../utils/exportCSV";
+
+const Topbar = ({ title, subTitle, jobs, onJobAdded, showExport = false }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [jobs, setJobs] = useState([]);
 
   const handleAddJob = (newJob) => {
-    setJobs([newJob, ...jobs]);
+    onJobAdded(newJob);
   };
+
   return (
     <div className="h-28 w-full bg-[#30302e] flex items-center justify-between px-4 text-white border-b border-b-white/16">
-      
       {/* Left side */}
       <div className="flex flex-col pl-5 gap-1">
-      <h1 className="text-xl font-semibold">{title}</h1>
-      <p className="text-[14px] text-[#888780]">{subTitle}</p>
+        <h1 className="text-xl font-semibold">{title}</h1>
+        <p className="text-[14px] text-[#888780]">{subTitle}</p>
       </div>
+
       {/* Right side */}
       <div className="flex gap-3 items-center pr-10">
-        <Button>Export CSV</Button>
+        {showExport && (
+  <Button onClick={() => exportJobsToCSV(jobs)}>
+    Export CSV
+  </Button>
+)}
+      
+
         <div>
-        <Button onClick={() => setIsOpen(true)}>+ Add Job</Button>
-        <AddJobModal
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        onJobAdded={handleAddJob}
-      />
+          <Button onClick={() => setIsOpen(true)}>+ Add Job</Button>
+
+          <AddJobModal
+            isOpen={isOpen}
+            onClose={() => setIsOpen(false)}
+            onJobAdded={handleAddJob}
+          />
+        </div>
       </div>
-      </div>
- 
     </div>
   );
 };
